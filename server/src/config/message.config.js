a840441e8200f18f9315e2b787e1e81e16e672be
Blinn Zachari <zacblinn@hotmail.fr>
2020-11-09 10:55:53 +0100

    fix

--- server/src/config/message.config.js
+++
@@ -1,8 +0,0 @@
-function server_state(type, status, port, env) {
-    this.type = type;
-    this.status = status;
-    this.port = port;
-    this.env = env;
-}
-
-exports.server = new server_state("SERVER", "OK", process.env.PORT, process.env.NODE_ENV);
