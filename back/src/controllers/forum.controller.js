aa0f7ebd997e43a4877b736b41b5ef58b95b4267
Zachari Blinn <zacblinn@hotmail.fr>
2021-01-20 17:59:42 +0100

    add crud forum

--- back/src/controllers/forum.controller.js
+++ back/src/controllers/forum.controller.js
@@ -33,3 +33,41 @@ exports.forum_findOne = async (req, res) => {
     res.status(500).json(`Error: ${error}`);
   }
 };
+
+exports.forum_find = async (req, res) => {
+  try {
+    const forum = await Forum.find();
+
+    if (!forum) throw new Error('There are no forum!');
+
+    res.status(200).json(forum);
+  } catch (error) {
+    res.status(500).json(`Error: ${error}`);
+  }
+};
+
+exports.forum_update = async (req, res) => {
+  try {
+    const forum = await Forum.findOneAndUpdate({ _id: req.params.id }, {
+      $set: req.body,
+    });
+
+    if (!forum) throw new Error('Forum not found!');
+
+    res.status(200).json(forum);
+  } catch (error) {
+    res.status(500).json(`Error: ${error}`);
+  }
+};
+
+exports.forum_delete = async (req, res) => {
+  try {
+    const forum = await Forum.findOneAndDelete({ _id: req.params.id });
+
+    if (!forum) throw new Error('Forum not found!');
+
+    res.status(200).json(forum);
+  } catch (error) {
+    res.status(500).json(`Error: ${error}`);
+  }
+};
