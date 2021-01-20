e20853b3986f0b25f42f263c1c33ac83115237f0
Zachari Blinn <zacblinn@hotmail.fr>
2021-01-20 17:44:31 +0100

    add findOne function to forum controller

--- back/src/controllers/forum.controller.js
+++ back/src/controllers/forum.controller.js
@@ -3,7 +3,7 @@ const Forum = require('../models/forum.model');
 /**
  * @description Create a new Forum
  * @param {title, description} req
- * @param {*} res
+ * @param {Forum} res
  */
 exports.forum_create = async (req, res) => {
   try {
@@ -12,6 +12,23 @@ exports.forum_create = async (req, res) => {
     await newForum.save();
 
     res.status(201).json(newForum);
+  } catch (error) {
+    res.status(500).json(`Error: ${error}`);
+  }
+};
+
+/**
+ * @description Find one forum by id
+ * @param {id} req
+ * @param {Forum} res
+ */
+exports.forum_findOne = async (req, res) => {
+  try {
+    const forum = await Forum.findOne({ _id: req.params.id });
+
+    if (!forum) throw new Error('Forum not found!');
+
+    res.status(200).json(forum);
   } catch (error) {
     res.status(500).json(`Error: ${error}`);
   }
